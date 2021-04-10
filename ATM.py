7e4583ea05641e7574b3502607e05b6d3d54654f
Hbassa <78148298+Hbassa@users.noreply.github.com>
2021-04-10 14:14:44 +0100

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,79 +1,123 @@
 from datetime import datetime
+import random
 
 currentDate = datetime.now()
 
-customersList = ["Kuzayet", "Pam", "Ovie", "Akpan", "Idibia"]
-customersPassword = ["K1234", "P1234", "O1234", "A1234", "I1234"]
-customersBalance = [1000, 2000, 3000, 500, 2000]
+database = {}
 
+def init():
 
-name = input("What is your name? \n")
+        print("Welcome To Bank Py")
 
-while name in customersList:
-    password = input("What is your password? \n")
-    userId = customersList.index(name)
+        haveAccount = int(
+            input("Do you have and account with us? 1. (Yes) 2. (No) \n"))
 
-    if password == customersPassword[userId]:
-        print("Welcome %s" % name)
+        if haveAccount == 1:
+            login()
+        elif haveAccount == 2:
+            register()
+        else:
+            print("You have selected an invalid option")
+            init()
 
+def login():
+        print("*****login*****")
+
+        accountNumberFromUser = int(input("What is your account number? \n"))
+        password = input("what is your password? \n")
+
+        for accountNumber,userDetails in database.items():
+            if accountNumber == accountNumberFromUser:
+                if userDetails[3] == password:
+                    bankOperation(userDetails)
+            else:
+                print("invalid account number")
+        login()
+
+def register():
+
+        print("*****Register*****")
+        email = input("what is your email address? \n")
+        first_name = input("what is your first name? \n")
+        last_name = input("what is your last name? \n")
+        password = input("generate a password for yourself \n")
+        balance = int(input("how much do you want to deposit into your new account? \n"))
+        accountNumber = generatingAccountNumber()
+
+        database[accountNumber] = [email, first_name, last_name, password, balance]
+
+        print("your account has been generated")
+        print("================================")
+        print("your account number is %d:" % accountNumber)
+        print("================================")
+        print("keep it safe")
+
+        login()
+
+def bankOperation(user):
+
+        print("welcome %s %s" % (user[0], user[1]))
         print("Time: %s" % currentDate)
 
-        print("1. Withdraw")
-        print("These are the available options:")
-        print("2. Deposit")
-        print("3. Check Balance")
-        print("4. Complaint")
-
-        selectedOption = int(input("please select an option: "))
+        selectedOption = int(input("what would you like to do? 1. (deposit) 2. (withdraw) 3. (check balance) 4. (complaint) 5. (logout) 6. (exit) \n"))
 
         if selectedOption == 1:
-            withdraw = int(
-                input("How much would you like to withdraw? \n"))
-            userBalance = customersList.index(name)
-
-            if customersBalance[userBalance] > withdraw:
-                customersBalance[userBalance] = customersBalance[userBalance] - withdraw
-
-                print("Take your cash")
-
-            else:
-                print("Insufficient Balance")
+            depositOperation(user)
 
         elif selectedOption == 2:
-            deposit = int(input("How much would you like to deposit? \n"))
-            userBalance = customersList.index(name)
-
-            customersBalance[userBalance] = customersBalance[userBalance] + deposit
-            print("Deposit successful")
+            withdrawOperation(user)
 
         elif selectedOption == 3:
-            userBalance = customersList.index(name)
-            print("Your balance is %s" % customersBalance[userBalance])
+            balanceOperation(user)
 
         elif selectedOption == 4:
-            input("What issue will you like to report? \n")
+            complaint()
 
-            print("Thank you for contacting us")
+        elif selectedOption == 5:
+            logout()     
+
+        elif selectedOption == 6:
+            exit()
 
         else:
-            print("Option selected is invalid, please try again")
+            print("invalid option selected")
+            bankOperation(user)
 
-    else:
-        print("Password is incorrect, please try again")
-        break
+def depositOperation(user):
+        print("*****deposit*****")
+        deposit = int(input("How much do you want to deposit? \n"))
+        user[4] = user[4] + deposit
 
-    print("Do you want to perform another transaction?")
-    print("1. Yes")
-    print("2. No")
+        print("Deposit Successful")
 
-    anotherTranscation = int(input("Please select an option: \n"))
+def withdrawOperation(user):
+        print("*****withdraw****")
+        withdraw = int(input("How much would you like to withdraw? \n"))
+        
+        if user[4] > withdraw:
+            user[4] = user[4] - withdraw
+            print("Take your cash")
 
-    if anotherTranscation == 1:
-        continue
+        else:
+            print("Insufficient Balance")
 
-    else:
-        break
+def balanceOperation(user):
+        print("****balance****")
+
+        print("your balance is %d" % user[4])
+
+def complaint():
+        input("What issue will you like to report? \n")
+
+        print("Thank you for contacting us")
+
+def logout():
+        login()
 
 
-else:
-    print("Name not found, please try again")
+def generatingAccountNumber():
+        return random.randrange(1111111111, 9999999999)
+
+
+
+init()
